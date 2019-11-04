1918494aa4173b2833addcd5b0da17f214706c5e
yfd <fudong.yang@socialcredits.cn>
2019-11-04 17:42:25 +0800

    template

---
+++ src/com/foo/pattern/template/HummerModel.java
@@ -0,0 +1,11 @@
+package com.foo.pattern.template;
+
+public abstract class HummerModel {
+    abstract void start();
+
+    abstract void alarm();
+
+    abstract void stop();
+
+    abstract void run();
+}
