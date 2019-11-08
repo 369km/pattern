512e731c5a4ea51a493d1306d4e717225a81be31
yfd <fudong.yang@socialcredits.cn>
2019-11-08 13:48:41 +0800

    proxy

---
+++ src/com/foo/pattern/proxy/GamePlayerProxy.java
@@ -0,0 +1,24 @@
+package com.foo.pattern.proxy;
+
+public class GamePlayerProxy implements IGamePlayer {
+    private IGamePlayer gamePlayer;
+
+    public GamePlayerProxy(IGamePlayer gamePlayer) {
+        this.gamePlayer = gamePlayer;
+    }
+
+    @Override
+    public void login(String user, String password) {
+        this.gamePlayer.login(user,password);
+    }
+
+    @Override
+    public void killBoss() {
+        this.gamePlayer.killBoss();
+    }
+
+    @Override
+    public void upgrade() {
+        this.gamePlayer.upgrade();
+    }
+}
---
+++ src/com/foo/pattern/proxy/IGamePlayer.java
@@ -0,0 +1,9 @@
+package com.foo.pattern.proxy;
+
+public interface IGamePlayer {
+    void login(String user, String password);
+
+    void killBoss();
+
+    void upgrade();
+}
